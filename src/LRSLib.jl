83791f77c774984fd8fcbb268fa479f070cfa761
bzinberg <bzinberg@mit.edu>
2021-05-11 16:00:50 -0400

    Migrate from BinDeps to BinaryBuilder / JLL (#40)
    * Migrate from BinDeps to BinaryBuilder / JLL, except the nash part
    
    * Remove `BinDeps` and `Homebrew` dependencies (fixes https://github.com/JuliaPolyhedra/LRSLib.jl/issues/38)
    * Add `lrslib_jll` dependency
    * Delete `deps/` (it's no longer used)
    
    * make method signature more specific, so that there is no ambiguity with its Polyhedra.jl parent
    
    * dummy commit to trigger CI re-run
    
    * bump Polyhedra.jl dependency to 0.6.14
    
    * Require lrslib_jll >= 0.2.0
    
    See https://github.com/JuliaPackaging/Yggdrasil/pull/2875
    
    * No need to specialize to HIndex anymore
    
    See discussion in https://github.com/JuliaPolyhedra/LRSLib.jl/pull/40#discussion_r618711213
    
    * Require lrslib_jll == 0.2.0
    
    See https://github.com/JuliaPolyhedra/LRSLib.jl/pull/40#discussion_r618712598

--- src/LRSLib.jl
+++ src/LRSLib.jl
@@ -1,15 +1,9 @@
 module LRSLib
 
-using BinDeps
 using Polyhedra
 using LinearAlgebra
+using lrslib_jll
 using Markdown
-
-if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
-    include("../deps/deps.jl")
-    else
-    error("LRSLib not properly installed. Please run Pkg.build(\"LRSLib\")")
-end
 
 macro lrs_ccall(f, args...)
     quote
