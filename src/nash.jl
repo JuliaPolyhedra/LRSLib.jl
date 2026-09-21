7cb37d140dc224772893c37a779956780335bc89
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2026-09-21 14:09:13 +0900

    Relax the type restriction in `nashsolve` and `buildrep`

--- src/nash.jl
+++ src/nash.jl
@@ -2,7 +2,7 @@
 Julia wrapper/translation of lrs_solve_nash
 
 =#
-const RatOrInt = Union{Rational,Int}
+const RatOrInt = Union{Rational,Integer}
 
 """
     nashsolve(A::AbstractMatrix, B::AbstractMatrix)
@@ -213,7 +213,8 @@ function buildrep(player_idx::Integer,
     nonnegativity_subarray[diagind(nonnegativity_subarray)] .= 1
 
     # FillConstraintRows
-    constraint_subarray[:, 1:end-1] = -opponent_payoff_matrix
+    constraint_subarray[:, 1:end-1] = opponent_payoff_matrix
+    constraint_subarray[:, 1:end-1] .*= -1
     constraint_subarray[:, end] .= 1
 
     # FillLinearityRow
